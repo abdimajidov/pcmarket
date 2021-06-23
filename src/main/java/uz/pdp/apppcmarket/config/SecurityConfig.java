044629e1b03ff4e7f2bc9f4290286b616b38f7f3
sardor <abdimajidovsardorbek@gmail.com>
2021-06-23 15:03:15 +0500

    SecurityConfig3

--- src/main/java/uz/pdp/apppcmarket/config/SecurityConfig.java
+++ src/main/java/uz/pdp/apppcmarket/config/SecurityConfig.java
@@ -1,11 +1,11 @@
 package uz.pdp.apppcmarket.config;
 
-import com.sun.org.apache.bcel.internal.generic.PUTFIELD;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth
                 .inMemoryAuthentication()
                 .withUser("superAdmin").password(passwordEncoder().encode("s123")).roles("SUPER_ADMIN").
-                authorities("READ_ALL_PRODUCT","READ_ONE_PRODUCT","READ_BY_FILTR","ADD_PRODUCT","EDIT_PRODUCT","DELETE_PRODUCT","ORDER")
+                authorities("READ_ALL_PRODUCT","READ_ONE_PRODUCT","READ_BY_FILTR","ADD_PRODUCT","EDIT_PRODUCT","DELETE_PRODUCT","ORDER","ALL")
                 .and()
                 .withUser("moderator").password(passwordEncoder().encode("m123")).roles("MODERATOR").
                 authorities("ADD_PRODUCT","EDIT_PRODUCT")
@@ -37,13 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/api/product/**").hasAuthority("ADD_PRODUCT")
                 .antMatchers(HttpMethod.PUT, "/api/product/**").hasAnyAuthority("EDIT_PRODUCT")
                 .antMatchers( "/api/orders/**").hasAuthority("ORDER")
-                .antMatchers("/api/attachment/**").hasRole("SUPER_ADMIN")
-                .antMatchers("/api/basketProduct/**").hasRole("SUPER_ADMIN")
-                .antMatchers("/api/basket/**").hasRole("SUPER_ADMIN")
-                .antMatchers("/api/category/**").hasRole("SUPER_ADMIN")
-                .antMatchers("/api/character/**").hasRole("SUPER_ADMIN")
-                .antMatchers("/api/customer/**").hasRole("SUPER_ADMIN")
-                .antMatchers("/api/property/**").hasRole("SUPER_ADMIN")
+                .antMatchers("/api/**").hasAuthority("ALL")
 
                 .anyRequest()
                 .authenticated()
